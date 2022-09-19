141745c74564ee5169f83254768af3e82d4b8e15
hebaashraf21 <heba.elmaaboud00@eng-st.cu.edu.eg>
2022-09-19 21:25:54 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+ return Center(
+ child: Text('Hello World Travel',
+ textDirection: TextDirection.ltr,),
+ );
+ }
+}
